a9c58d82122110821153fceef95adeb0ea36fe53
zapagenrevdale <zapagenrevdale@gmail.com>
2024-04-16 22:31:29 +0800

    add meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-"use client";
-import {useClientAuth} from "@/hooks/use-client-auth";
-import SearchForm from "@/components/atoms/search-photo";
 import Image from "next/image";
 import {Suspense} from "react";
 import HomePageSearchForm from "@/components/molecules/homepage-search-form";
+import {Metadata} from 'next';
 
+export const metadata: Metadata = {
+    title: "Welcome page | AetherLenz",
+    description: "Look for photography captains, ask for majestic shots, and find your photos here in seconds."
+};
 
 const CardInfo: React.FC<{ title: string; url: string; order: number; }> = ({title, url, order}) => {
     return <div className="w-fit flex items-center flex-col group  hover:scale-105">
@@ -19,40 +21,33 @@ const CardInfo: React.FC<{ title: string; url: string; order: number; }> = ({tit
 }
 
 export default function Home() {
-    const ui = useClientAuth();
-    return (
-        <main className="container min-h-screen p-24">
-
-            {/* hero banner */}
-            <div className="relative w-full">
-                <div className="flex items-center flex-col text-center ">
-                    <h1 className="text-6xl font-bold leading-normal text-sky-500">
-                        Discover, Capture, Inspire
-                    </h1>
-                    <h1 className="text-6xl font-bold leading-normal text-sky-500">
-                        with AetherLenz
-                    </h1>
-                </div>
-                <div className="flex justify-center text-center p-3 my-5">
-                    <h3 className="text-2xl  leading-loose text-neutral-800">
-                        Look for photography captains, ask for majestic shots, and find your photos here in seconds.
-                    </h3>
-                </div>
-                {/*<div className="absolute h-[450px] w-[29vw] right-0 -top-6 -z-10 rounded-xl overflow-hidden">*/}
-                {/*    <Image src="/images/bg.jpg" alt="background image" fill className="object-cover"/>*/}
-                {/*</div>*/}
+    return <main className="container min-h-screen p-24">
+        {/* hero banner */}
+        <div className="relative w-full">
+            <div className="flex items-center flex-col text-center ">
+                <h1 className="text-6xl font-bold leading-normal text-sky-500">
+                    Discover, Capture, Inspire
+                </h1>
+                <h1 className="text-6xl font-bold leading-normal text-sky-500">
+                    with AetherLenz
+                </h1>
             </div>
-
-            <Suspense>
-                <HomePageSearchForm/>
-            </Suspense>
-
-            <div className="w-full flex justify-between items-end mt-4">
-                <CardInfo url="/images/register.jpg" title="Sign up for free!" order={1}/>
-                <CardInfo url="/images/photographer.jpg" title="Approach our photography captains"
-                          order={2}/>
-                <CardInfo url="/images/searched-photo.jpg" title="Find photos with your face." order={3}/>
+            <div className="flex justify-center text-center p-3 my-5">
+                <h3 className="text-2xl  leading-loose text-neutral-800">
+                    Look for photography captains, ask for majestic shots, and find your photos here in seconds.
+                </h3>
             </div>
-        </main>
-    );
+        </div>
+
+        <Suspense>
+            <HomePageSearchForm/>
+        </Suspense>
+
+        <div className="w-full flex justify-between items-end mt-4">
+            <CardInfo url="/images/register.jpg" title="Sign up for free!" order={1}/>
+            <CardInfo url="/images/photographer.jpg" title="Approach our photography captains"
+                      order={2}/>
+            <CardInfo url="/images/searched-photo.jpg" title="Find photos with your face." order={3}/>
+        </div>
+    </main>
 }
